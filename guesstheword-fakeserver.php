ab0fef5f2bf315913ba356191fa91e2006508fbc
Emil <emil.sall@oakwood.se>
2012-10-20 14:57:35 +0200

    Guess the word client-side

---
+++ guesstheword-fakeserver.php
@@ -0,0 +1,13 @@
+<?php 
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: application/json');
+
+if(isset($_GET['guess'])) {
+	$Response = array('true', 'false');
+	echo $Response[rand(0,1)];
+} else {
+	echo 'fsdfsdf,ertertert,fhffb';
+}
+
+?>
